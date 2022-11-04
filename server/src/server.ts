a59d44c88bf1f8ffb9e646f72c3613aac34687f6
Jaum Vitor <joao.vitor.pires.060@gmail.com>
2022-11-03 23:43:26 -0300

    create endpoint count, users, guesses, pools. And then, create router post for add pools

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 // configurando para fazer manipulações no BD
 const prisma = new PrismaClient({
@@ -17,10 +19,47 @@ async function bootstrap(){
     origin: true
   })
 
-  fastify.get('/pools/count', async () => {
+  fastify.get('/pools/count', async (request, reply) => {
     const count = await prisma.pool.count()
 
-    return { count }
+    return reply.status(200).send( { count } )
+  })
+
+  fastify.get('/guesses/count', async (request, reply) => {
+    const count = await prisma.guess.count()
+
+    return reply.status(200).send( { count } )
+  })
+
+  fastify.get('/users/count', async (request, reply) => {
+    const count = await prisma.user.count()
+    
+    return reply.status(200).send( { count } )
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    // Criando formato da minha request
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+
+    // Vai tentar converter para o formato que criei, se não der dispara uma exception
+    try {
+      const { title } = createPoolBody.parse(request.body)
+      const generate = new ShortUniqueId({ length: 6 })
+      const code = String(generate()).toUpperCase()
+
+      await prisma.pool.create({
+        data: {
+          title: title,
+          code: code
+        }
+      })
+
+      return reply.status(201).send({ code: code })
+    }catch (e){
+      return reply.status(400).send({ error: 'Value null' })
+    }
   })
 
   await fastify.listen({ port: 8080, /*host: '0.0.0.0'*/ })
